54e9f399d5b3b8994ac5c7897d34a67d6d3a1f10
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-12-03 12:37:03 -0600

    created routes for pages of app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
 import React from 'react';
+import Main from './components/Main';
 import './App.css';
+
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="demo-big-content">
-        <Layout>
-            <Header title="Title" scroll>
-                <Navigation>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                </Navigation>
-            </Header>
-            <Drawer title="Title">
-                <Navigation>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                    <a href="/">Link</a>
-                </Navigation>
-            </Drawer>
-            <Content>
-                <div className="page-content" />
-            </Content>
-        </Layout>
+      <Layout>
+        <Header title="Title" scroll>
+          <Navigation>
+            <Link to="/AboutMe">About Me</Link>
+            <Link to="/Projects">Projects</Link>
+            <Link to="/Resume">Resume</Link>
+            <Link to="/Contact">Contact</Link>
+          </Navigation>
+        </Header>
+        <Drawer title="Title">
+          <Navigation>
+            <Link to="/AboutMe">About Me</Link>
+            <Link to="/Projects">Projects</Link>
+            <Link to="/Resume">Resume</Link>
+            <Link to="/Contact">Contact</Link>
+          </Navigation>
+        </Drawer>
+        <Content>
+          <div className="page-content" />
+          <Main />
+        </Content>
+      </Layout>
     </div>
   );
 }
