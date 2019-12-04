2b0af26978dcc0060c07ddfffb737497091bbd06
Nasser Atiyeh <nasseratiyeh@gmail.com>
2019-12-04 14:58:42 -0600

    added content to landing page and styled

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -13,6 +13,31 @@ class Landing extends Component {
               alt="avatar"
               className="avatar-img"
             />
+            <div className="banner-text">
+              <h1>Full Stack Web Developer</h1>
+
+              <hr />
+
+              <p>HTML/CSS | Bootstrap | JavaScript | React | React Native | NodeJS | Express | MongoDB</p>
+              <div className="social-links">
+                {/*LinkedIn*/}
+                <a href="https://www.linkedin.com/in/nasser-atiyeh-3758a6b8/" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+                {/*GitHub*/}
+                <a href="https://github.com/Natiyeh" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+                {/*Twitter*/}
+                <a href="https://twitter.com/Nassboss" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-twitter-square" aria-hidden="true" />
+                </a>
+                {/*Facebook*/}
+                <a href="https://www.facebook.com/nasser.atiyeh.9?ref=bookmarks" rel="noopener noreferrer" target="_blank">
+                  <i className="fa fa-facebook-square" aria-hidden="true" />
+                </a>
+              </div>
+            </div>
           </Cell>
         </Grid>
       </div>
